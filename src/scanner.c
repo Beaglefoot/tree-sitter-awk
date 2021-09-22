cfd7755a0a4e2ef37e107b3562f906ec33138b83
Beaglefoot <ch-e-st@yandex.ru>
2021-09-22 22:25:06 +0300

    Fix if-else with multiline comments

--- src/scanner.c
+++ src/scanner.c
@@ -17,58 +17,7 @@ void log(TSLexer *lexer)
          lexer->is_at_included_range_start(lexer) ? "true" : "false");
 }
 
-void *tree_sitter_awk_external_scanner_create()
-{
-  return NULL;
-}
-
-void tree_sitter_awk_external_scanner_destroy(void *payload)
-{
-}
-
-unsigned tree_sitter_awk_external_scanner_serialize(void *payload, char *state)
-{
-  return 0;
-}
-
-void tree_sitter_awk_external_scanner_deserialize(void *payload, const char *state, unsigned length)
-{
-}
-
-bool tree_sitter_awk_external_scanner_scan(void *payload, TSLexer *lexer,
-                                           const bool *valid_symbols)
-{
-  bool statement_terminator_was_found = false;
-
-  if (valid_symbols[_IF_ELSE_SEPARATOR])
-  {
-    skip_whitespace(lexer, false);
-
-    if (is_statement_terminator(lexer->lookahead))
-    {
-      statement_terminator_was_found = true;
-    }
-
-    if (is_if_else_separator(lexer))
-    {
-      lexer->result_symbol = _IF_ELSE_SEPARATOR;
-      return true;
-    }
-  }
-
-  if (valid_symbols[CONCATENATING_SPACE] && !statement_terminator_was_found)
-  {
-    if (is_concatenating_space(lexer))
-    {
-      lexer->result_symbol = CONCATENATING_SPACE;
-      return true;
-    }
-  }
-
-  return false;
-}
-
-int next_chars_eq(TSLexer *lexer, char *word)
+bool next_chars_eq(TSLexer *lexer, char *word)
 {
   for (int i = 0; i < strlen(word); i++)
   {
@@ -82,12 +31,12 @@ int next_chars_eq(TSLexer *lexer, char *word)
   return true;
 }
 
-int is_whitespace(int32_t c)
+bool is_whitespace(int32_t c)
 {
   return c == ' ' || c == '\t';
 }
 
-int is_statement_terminator(int32_t c)
+bool is_statement_terminator(int32_t c)
 {
   return c == '\n' || c == ';';
 }
@@ -105,17 +54,24 @@ void skip_comment(TSLexer *lexer)
   }
 
   lexer->advance(lexer, true);
+
+  skip_whitespace(lexer, true, false);
+
+  if (lexer->lookahead == '#')
+  {
+    skip_comment(lexer);
+  }
 }
 
-void skip_whitespace(TSLexer *lexer, bool capture)
+void skip_whitespace(TSLexer *lexer, bool skip_newlines, bool capture)
 {
-  while (is_whitespace(lexer->lookahead))
+  while (is_whitespace(lexer->lookahead) || (skip_newlines && lexer->lookahead == '\n'))
   {
     lexer->advance(lexer, !capture);
   }
 }
 
-int is_if_else_separator(TSLexer *lexer)
+bool is_if_else_separator(TSLexer *lexer)
 {
   while (is_whitespace(lexer->lookahead) || is_statement_terminator(lexer->lookahead))
   {
@@ -127,15 +83,15 @@ int is_if_else_separator(TSLexer *lexer)
   if (lexer->lookahead == '#')
   {
     skip_comment(lexer);
-    skip_whitespace(lexer, false);
+    skip_whitespace(lexer, false, false);
   }
 
   return next_chars_eq(lexer, "else");
 }
 
-int is_concatenating_space(TSLexer *lexer)
+bool is_concatenating_space(TSLexer *lexer)
 {
-  skip_whitespace(lexer, true);
+  skip_whitespace(lexer, false, true);
 
   lexer->mark_end(lexer);
 
@@ -177,4 +133,55 @@ int is_concatenating_space(TSLexer *lexer)
   default:
     return true;
   }
+}
+
+void *tree_sitter_awk_external_scanner_create()
+{
+  return NULL;
+}
+
+void tree_sitter_awk_external_scanner_destroy(void *payload)
+{
+}
+
+unsigned tree_sitter_awk_external_scanner_serialize(void *payload, char *state)
+{
+  return 0;
+}
+
+void tree_sitter_awk_external_scanner_deserialize(void *payload, const char *state, unsigned length)
+{
+}
+
+bool tree_sitter_awk_external_scanner_scan(void *payload, TSLexer *lexer,
+                                           const bool *valid_symbols)
+{
+  bool statement_terminator_was_found = false;
+
+  if (valid_symbols[_IF_ELSE_SEPARATOR])
+  {
+    skip_whitespace(lexer, false, false);
+
+    if (is_statement_terminator(lexer->lookahead))
+    {
+      statement_terminator_was_found = true;
+    }
+
+    if (is_if_else_separator(lexer))
+    {
+      lexer->result_symbol = _IF_ELSE_SEPARATOR;
+      return true;
+    }
+  }
+
+  if (valid_symbols[CONCATENATING_SPACE] && !statement_terminator_was_found)
+  {
+    if (is_concatenating_space(lexer))
+    {
+      lexer->result_symbol = CONCATENATING_SPACE;
+      return true;
+    }
+  }
+
+  return false;
 }
